c605310c4af6fefec24ab6243b489cfe425c11af
yuu8887 <76433094+yuu8887@users.noreply.github.com>
2026-07-12 20:45:04 +0900

    Update SF6_Framedata_ExcelDataManager.py

--- SF6_Framedata_ExcelDataManager.py
+++ SF6_Framedata_ExcelDataManager.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-def __init__(self, filename):
+class ExcelDataManager:
+    def __init__(self, filename):
 
         # Excelを読み込む
         self.df = pd.read_excel(
@@ -11,30 +12,30 @@ def __init__(self, filename):
         # 空欄を空文字にする
         self.df = self.df.fillna("")
 
-        # 全列をPython標準の文字列(str)へ変換
+        # 全セルを文字列へ変換
         for col in self.df.columns:
 
             self.df[col] = self.df[col].apply(self.convert_value)
 
-        # 列名も念のためstrへ変換
+        # 列名も文字列へ
         self.df.columns = [
             str(col)
             for col in self.df.columns
         ]
 
-def convert_value(self, value):
+    def convert_value(self, value):
 
-    if pd.isna(value):
-        return ""
+        if pd.isna(value):
+            return ""
 
-    if isinstance(value, float):
+        if isinstance(value, float):
 
-        if value.is_integer():
-            return str(int(value))
+            if value.is_integer():
+                return str(int(value))
 
-    return str(value)
+        return str(value)
 
 
-def get_dataframe(self):
+    def get_dataframe(self):
 
         return self.df.copy()
