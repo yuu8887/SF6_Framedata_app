867a3555ab4d78ea577cd87c7510b4a3532cb025
yuu8887 <76433094+yuu8887@users.noreply.github.com>
2026-07-12 19:40:36 +0900

    Update SF6_Framedata_ExcelDataManager.py

--- SF6_Framedata_ExcelDataManager.py
+++ SF6_Framedata_ExcelDataManager.py
@@ -5,5 +5,8 @@ class ExcelDataManager:
     def __init__(self, filename):
         self.df = pd.read_excel(filename)
 
+        self.df = self.df.fillna("")
+        self.df = self.df.astype(str)
+
     def get_dataframe(self):
         return self.df
