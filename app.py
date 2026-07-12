39ad19797faa889d530464e697392c2eb56522a3
yuu8887 <76433094+yuu8887@users.noreply.github.com>
2026-07-12 18:01:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -87,7 +87,7 @@ if st.button("検索"):
         command=command,
         cancel=cancel,
         zokusei=zokusei,
-#        startup=startup,
+        startup=startup,
         hitF=hitF,
         guardF=guardF
     )
