b7f9cbf085ce832b558434cf539b6a5ac173d6b2
yuu8887 <76433094+yuu8887@users.noreply.github.com>
2026-07-12 20:30:08 +0900

    Update SF6_Framedata_ExcelDataManager.py

--- SF6_Framedata_ExcelDataManager.py
+++ SF6_Framedata_ExcelDataManager.py
@@ -1,12 +1,32 @@
 import pandas as pd
 
-class ExcelDataManager:
+def __init__(self, filename):
 
-    def __init__(self, filename):
-        self.df = pd.read_excel(filename)
+        # Excelを読み込む
+        self.df = pd.read_excel(
+            filename,
+            dtype=object
+        )
 
+        # 空欄を空文字にする
         self.df = self.df.fillna("")
-        self.df = self.df.astype(str)
 
-    def get_dataframe(self):
-        return self.df
+        # 全列をPython標準の文字列(str)へ変換
+        for col in self.df.columns:
+
+            self.df[col] = (
+                self.df[col]
+                .apply(str)
+                .str.replace(".0", "", regex=False)
+            )
+
+        # 列名も念のためstrへ変換
+        self.df.columns = [
+            str(col)
+            for col in self.df.columns
+        ]
+
+
+def get_dataframe(self):
+
+        return self.df.copy()
