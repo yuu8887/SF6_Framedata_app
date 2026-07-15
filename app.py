96aa34e525f58da80cf16ca1d917fd374c303809
yuu8887 <76433094+yuu8887@users.noreply.github.com>
2026-07-15 20:34:15 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,35 @@
 import streamlit as st
-from streamlit_gtag import st_gtag
+import streamlit_analytics2 as streamlit_analytics
 
-st.set_page_config(
-    page_title="SF6 フレームデータビューワー",
-    layout="wide"
-)
+with streamlit_analytics.track():
 
-st_gtag(
-    key="gtag",
-    id="G-ZJ72KNDK75",
-    event_name="page_view",
-    params={
-        "event_category": "navigation",
-        "event_label": "home",
-        "value": 1,
-    },
-)
+    st.set_page_config(
+        page_title="SF6 フレームデータビューワー",
+        layout="wide"
+    )
 
-from SF6_Framedata_ExcelDataManager import ExcelDataManager
-from SF6_Framedata_FilterController import FilterController
 
-###################################################
-# データ読み込み
-###################################################
+    from SF6_Framedata_ExcelDataManager import ExcelDataManager
+    from SF6_Framedata_FilterController import FilterController
 
-manager = ExcelDataManager("SF6_FrameData.xlsx")
+    ###################################################
+    # データ読み込み
+    ###################################################
 
-df = manager.get_dataframe()
+    manager = ExcelDataManager("SF6_FrameData.xlsx")
 
-controller = FilterController(df)
+    df = manager.get_dataframe()
 
-###################################################
-# タイトル
-###################################################
-st.subheader("SF6 フレームデータビューワー")
+    controller = FilterController(df)
 
-with st.expander("📖 使い方を表示"):
+    ###################################################
+    # タイトル
+    ###################################################
+    st.subheader("SF6 フレームデータビューワー")
 
-    st.markdown("""
+    with st.expander("📖 使い方を表示"):
+
+        st.markdown("""
 #### 基本操作
 
 ① キャラクターを選択します。
@@ -85,149 +77,149 @@ with st.expander("📖 使い方を表示"):
 ・誤記などは修正したいと思いますが、機能追加については基本的に対応不可となります。
 """)
 
-###################################################
-# キャラクター選択
-###################################################
+    ###################################################
+    # キャラクター選択
+    ###################################################
 
-col1, col2, col3, col4 = st.columns(4)
+    col1, col2, col3, col4 = st.columns(4)
 
-with col1:
+    with col1:
 
-    character = st.selectbox(
-        "キャラクター",
-        controller.get_character1_list(),
- #       key="character"
+        character = st.selectbox(
+            "キャラクター",
+            controller.get_character1_list(),
+ #           key="character"
+        )
+
+        character2 = st.selectbox(
+            "キャラクター(比較用)",
+            controller.get_character2_list(),
+ #           key="character2"
+        )
+        operation = st.selectbox(
+            "操作タイプ",
+            controller.get_operation_type_list(),
+ #            key="operation"
+        )
+
+
+    with col2:
+
+        movetype = st.selectbox(
+            "技タイプ",
+            controller.get_movetype_type_list(),
+ #           key="movetype"
+        )
+
+        cancel = st.selectbox(
+            "キャンセル",
+            controller.get_cancel_list(),
+ #           key="cancel"        
+        )
+
+        zokusei = st.selectbox(
+            "属性",
+            controller.get_zokusei_list(),
+ #           key="zokusei"
+        )
+
+
+    with col3:
+
+        command = st.selectbox(
+            "コマンド",
+            controller.get_command_list(),
+ #           key="command"
+        )
+
+        command2 = st.selectbox(
+            "コマンド(比較用)",
+            controller.get_command2_list(),
+ #           key="command2"
+        )
+
+    with col4:
+        startup = st.text_input(
+            "発生(数値入力)",
+            value="",
+ #           key="startup"
+        )
+
+        startup = startup.strip()
+        if startup == "":
+            startup = None
+
+
+        hitF = st.text_input(
+            "ヒット時硬直差(数値入力)",
+            value="",
+ #           key="hitF"
+        )
+
+        hitF = hitF.strip()
+        if hitF == "":
+            hitF = None
+
+
+        guardF = st.text_input(
+            "ガード時硬直差(数値入力)",
+            value="",
+ #           key="guardF"
+        )
+
+        guardF = guardF.strip()
+        if guardF == "":
+            guardF = None
+
+
+    ###################################################
+    # 検索ボタン
+    ###################################################
+
+    search = st.button("検索")
+
+
+    if "result" not in st.session_state:
+        st.session_state.result = df
+
+    if search:
+        st.session_state.result = controller.filter_data(
+            character1=character,
+            character2=character2,
+            operation=operation,
+            movetype=movetype,
+            command=command,
+            command2=command2,
+            cancel=cancel,
+            zokusei=zokusei,
+            startup=startup,
+            hitF=hitF,
+            guardF=guardF
+        )
+
+
+
+    ###################################################
+    # 表示
+    ###################################################
+
+    display_df = st.session_state.result.copy()
+
+    display_df = display_df.fillna("")
+
+    display_df = display_df.astype(str)
+
+    display_df = display_df.rename(columns={
+        "D増加量(ヒット)": "D増加量\n(ヒット)",
+        "D増加量(ガード)": "D増加量\n(ガード)",
+        "D増加量(パニカン)": "D増加量\n(パニカン)",
+        "ヒット時硬直差": "ヒット時\n硬直差",
+        "ガード時硬直差": "ガード時\n硬直差",
+    })
+
+    st.dataframe(
+        display_df,
+        width="stretch"
     )
 
-    character2 = st.selectbox(
-        "キャラクター(比較用)",
-        controller.get_character2_list(),
-#        key="character2"
-    )
-    operation = st.selectbox(
-        "操作タイプ",
-        controller.get_operation_type_list(),
-#        key="operation"
-    )
-
-
-with col2:
-
-    movetype = st.selectbox(
-        "技タイプ",
-        controller.get_movetype_type_list(),
-#        key="movetype"
-    )
-
-    cancel = st.selectbox(
-        "キャンセル",
-        controller.get_cancel_list(),
-#        key="cancel"        
-    )
-
-    zokusei = st.selectbox(
-        "属性",
-        controller.get_zokusei_list(),
-#        key="zokusei"
-    )
-
-
-with col3:
-
-    command = st.selectbox(
-        "コマンド",
-        controller.get_command_list(),
-#        key="command"
-    )
-
-    command2 = st.selectbox(
-        "コマンド(比較用)",
-        controller.get_command2_list(),
-#        key="command2"
-    )
-
-with col4:
-    startup = st.text_input(
-        "発生(数値入力)",
-        value="",
-#        key="startup"
-    )
-
-    startup = startup.strip()
-    if startup == "":
-        startup = None
-
-
-    hitF = st.text_input(
-        "ヒット時硬直差(数値入力)",
-        value="",
-#        key="hitF"
-    )
-
-    hitF = hitF.strip()
-    if hitF == "":
-        hitF = None
-
-
-    guardF = st.text_input(
-        "ガード時硬直差(数値入力)",
-        value="",
-#        key="guardF"
-    )
-
-    guardF = guardF.strip()
-    if guardF == "":
-        guardF = None
-
-
-###################################################
-# 検索ボタン
-###################################################
-
-search = st.button("検索")
-
-
-if "result" not in st.session_state:
-    st.session_state.result = df
-
-if search:
-    st.session_state.result = controller.filter_data(
-        character1=character,
-        character2=character2,
-        operation=operation,
-        movetype=movetype,
-        command=command,
-        command2=command2,
-        cancel=cancel,
-        zokusei=zokusei,
-        startup=startup,
-        hitF=hitF,
-        guardF=guardF
-    )
-
-
-
-###################################################
-# 表示
-###################################################
-
-display_df = st.session_state.result.copy()
-
-display_df = display_df.fillna("")
-
-display_df = display_df.astype(str)
-
-display_df = display_df.rename(columns={
-    "D増加量(ヒット)": "D増加量\n(ヒット)",
-    "D増加量(ガード)": "D増加量\n(ガード)",
-    "D増加量(パニカン)": "D増加量\n(パニカン)",
-    "ヒット時硬直差": "ヒット時\n硬直差",
-    "ガード時硬直差": "ガード時\n硬直差",
-})
-
-st.dataframe(
-    display_df,
-    width="stretch"
-)
-
-st.write(f"検索結果：{len(st.session_state.result)}件")
+    st.write(f"検索結果：{len(st.session_state.result)}件")
